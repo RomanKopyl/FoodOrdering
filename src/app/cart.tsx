b32e414dd6929bf744717181b8836746290ac2ad
Roman <>
2024-07-12 13:41:48 +0300

    Added Total price

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FlatList, Platform, StyleSheet, View } from 'react-native';
+import { FlatList, Platform, StyleSheet, Text, View } from 'react-native';
+import Button from '../components/Button';
 import CartListItem from '../components/CartListItem';
 import { useCart } from '../providers/CartProvider';
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
 
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={styles.contentContainerStyle}
       />
+      <Text style={{
+        marginTop: 20,
+        fontSize: 20,
+      }}>Total: {total}</Text>
+      <Button text="Chechout" />
 
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
